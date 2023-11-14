36989b861742878d041cdc8cc5c34b009d1467bd
Bonzaii1 <mattherr23@gmail.com>
2023-11-13 23:30:42 -0600

    Finished base contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const formRef = useRef(null);
     const [form, setForm] = useState({ name: "", email: "", message: "" })
     const [isLoading, setIsLoading] = useState(false)
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
     }
 
     const handleFocus = () => {
@@ -15,11 +17,42 @@ const Contact = () => {
     const handleBlur = () => {
 
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        emailjs.send(
+            import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+            import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+            {
+                from_name: form.name,
+                to_name: "Matthew",
+                from_email: form.email,
+                to_email: import.meta.env.VITE_APP_EMAILJS_EMAIL,
+                message: form.message
+            },
+            import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+
+        ).then(() => {
+            setIsLoading(false);
+            // TODO: show success message
+            // TODO: hide and alert
+
+            setForm({ name: "", email: "", message: "" })
+        }).catch((error) => {
+            setIsLoading(false);
+            console.log(error)
+            //TODO : Show error message
+        })
+
+    }
     return (
         <section className='relative flex lg:flex-row flex-col max-container'>
             <div className='flex-1 min-w-[50%] flex flex-col'>
                 <h1 className='head-text'>Get in Touch!</h1>
-                <form className='w-full flex flex-col gap-7 mt-14'>
+                <form className='w-full flex flex-col gap-7 mt-14'
+                    onSubmit={handleSubmit}>
                     <label className='text-black-500 font-semibold'>Name</label>
                     <input
                         type='text'
