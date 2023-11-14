6e75006eb609b2502fbeea5e5617062097f07d71
Bonzaii1 <mattherr23@gmail.com>
2023-11-13 20:49:06 -0600

    finished raw rotation on island

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import planeScene from '../assets/3d/plane.glb'
-import { useGLTF } from '@react-three/drei'
+import { useAnimations, useGLTF } from '@react-three/drei'
 
 const Plane = ({ isRotating, ...props }) => {
     const { scene, animations } = useGLTF(planeScene)
+    const { actions } = useAnimations(animations, scene)
     return (
         <mesh {...props}>
             <primitive object={scene}></primitive>
